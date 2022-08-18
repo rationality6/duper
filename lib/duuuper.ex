b655b5040c70ba8f1a52f16751c3919124c5c5b6
Hyun Ahn <codepawn@icloud.com>
2022-08-18 15:05:24 +0900

    first commit

---
+++ lib/duuuper.ex
@@ -0,0 +1,18 @@
+defmodule Duuuper do
+  @moduledoc """
+  Documentation for `Duuuper`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Duuuper.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/duuuper_test.exs
@@ -0,0 +1,8 @@
+defmodule DuuuperTest do
+  use ExUnit.Case
+  doctest Duuuper
+
+  test "greets the world" do
+    assert Duuuper.hello() == :world
+  end
+end
